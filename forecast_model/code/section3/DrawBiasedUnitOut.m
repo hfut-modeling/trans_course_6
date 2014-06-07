db92032ae70c8b373a01aee06bbdd06c6b31aadc
王刚 <wgybzb@sina.cn>
2014-06-07 11:17:54 +0800

    首次提交

---
+++ forecast_model/code/section3/DrawBiasedUnitOut.m
@@ -0,0 +1,11 @@
+function DrawBiasedUnitOut(UnitOut)
+%% 子函数，建议放于独立的m文件中，命名为DrawBiasedUnitOut.m
+% 绘制标准差较小的单个隐节点输出
+[xxx,PtNum] = size(UnitOut);
+figure('Position',[300 300 400 300])
+echo off
+axis([0 PtNum 0 1])
+axis on
+grid
+hold on
+plot(1:PtNum,UnitOut,'k-')
